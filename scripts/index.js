b040c082f91c9f9c163f9700e9335410d3e1eefb
Alexey Delendik <volosya007@yandex.ru>
2020-06-14 20:21:10 +0300

    первый вариант 4 работы

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__button');
 let popupCloseButton = popup.querySelector('.popup__close');
 
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
 let popupSaveButton = popup.querySelector('.popup__save');
 
-let popupName = popup.querySelector('.popup__name');
-let popupAbout = popup.querySelector('.popup__about');
+const popupName = popup.querySelector('.popup__name');
+const popupAbout = popup.querySelector('.popup__about');
 
-let popupToggle = function() {
+const popupToggle = function() {
   popupName.value = profileName.textContent;
   popupAbout.value = profileAbout.textContent;
   popup.classList.toggle('popup__opened');
@@ -18,9 +18,7 @@ let popupToggle = function() {
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 
-
-
-let editProfile = function(){
+const editProfile = function(){
   profileName.textContent=popupName.value;
   profileAbout.textContent = popupAbout.value;
   popupToggle();
