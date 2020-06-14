941ded1f215393f985202ffbb8c4fc5969071b4e
Alexey Delendik <volosya007@yandex.ru>
2020-06-14 19:59:41 +0300

    активанция кнопки сохранить

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,31 @@ let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__button');
 let popupCloseButton = popup.querySelector('.popup__close');
 
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let popupSaveButton = popup.querySelector('.popup__save');
+
+let popupName = popup.querySelector('.popup__name');
+let popupAbout = popup.querySelector('.popup__about');
+
 let popupToggle = function() {
+  popupName.value = profileName.textContent;
+  popupAbout.value = profileAbout.textContent;
   popup.classList.toggle('popup__opened');
-  console.log(popup);
 }
 
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
+
+
+
+let editProfile = function(){
+  profileName.textContent=popupName.value;
+  profileAbout.textContent = popupAbout.value;
+  popupToggle();
+}
+
+popupSaveButton.addEventListener('click', editProfile);
+
+
+
