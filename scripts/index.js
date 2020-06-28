80e63b3d74abb0613dd71de7cdf4e290aa77e033
Alexey Delendik <volosya007@yandex.ru>
2020-06-28 15:55:33 +0300

    InitialCards

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,75 @@
-const popup = document.querySelector('.popup');
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const templateCard = document.querySelector('.template-card');
+const cardList = document.querySelector('.cards-list');
+
+
+function addCard(card) {
+  const cards = templateCard.content.cloneNode(true);
+  const cardTitle = cards.querySelector('.card__title');
+  const cardLink = cards.querySelector('.card__picture');
+  cardTitle.textContent = card.name;
+  cardLink.src = card.link;
+  //addTodoListeners(todo);
+
+  cardList.append(cards);
+}
+
+if(initialCards==false){
+  document.querySelector('.cards-list__empty').style.display='flex';
+  document.querySelector('.cards-list').style.display='flex';
+}else{
+  initialCards.forEach(card=>{
+    addCard(card);
+  });
+}
+
+const popupEditProfile = document.querySelector('.popupEditProfile');
 let popupOpenButton = document.querySelector('.profile__button');
-let popupCloseButton = popup.querySelector('.popup__close');
+let popupCloseButton = popupEditProfile.querySelector('.popup__close');
 
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-let popupSaveButton = popup.querySelector('.popup__save');
+let popupSaveButton = popupEditProfile.querySelector('.popup__save');
 
-const popupName = popup.querySelector('.popup__name');
-const popupAbout = popup.querySelector('.popup__about');
+const popupName = popupEditProfile.querySelector('.popup__name');
+const popupAbout = popupEditProfile.querySelector('.popup__about');
 
 const popupToggle = function() {
   popupName.value = profileName.textContent;
   popupAbout.value = profileAbout.textContent;
-  popup.classList.toggle('popup__opened');
+  popupEditProfile.classList.toggle('popup__opened');
 }
 
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 
-let formElement = popup.querySelector('.popup__form');
+let formElement = popupEditProfile.querySelector('.popup__form');
 
 const editProfile = function(evt){
   evt.preventDefault();
@@ -29,6 +80,5 @@ const editProfile = function(evt){
 
 formElement.addEventListener('submit', editProfile);
 popupSaveButton.addEventListener('click', editProfile);
-
 
 
